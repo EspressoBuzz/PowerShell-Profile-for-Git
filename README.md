9a703907d79263ae14410ae078a78523986fc8a0
Frank Roberto <v-frroberto@microsoft.com>
2023-09-15 09:43:50 -0700

    main: added 2 more functions

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # PowerShell-Profile-for-Git
 
 I got tired of typing a few Git commands, and wanted to alias them.  
-"git status"         -->  "gs"  
-"git log --oneline"  -->  "glo"  
+"git status"          -->  "gs"  
+"git log --oneline"   -->  "glo"  
+"git branch --all"    -->  "gba"  
 
 Although you can create aliases in Git, they would still be "git \<alias>".   I wanted to type just a few characters.  
 Although you can create aliases in PowerShell, PowerShell aliases can't contain spaces.
 
-So I made these 2 small functions.
+So I made these small functions.
 
 Now I don't have to type these commands out a million times any more.
 
--- profile.ps1
+++ profile.ps1
@@ -1,11 +1,30 @@
-# Two PowerShell functions which alias Git commands.
+# PowerShell functions which alias Git commands, and touch().
 
+# git Status
 function gs(){
 	write-host ' '   # for blank line
     & git status
 }
 
+# git log --oneline
 function glo(){
     Write-Host ' '     # for blank line
     & git log --oneline
+}
+
+# git branch --all
+function gba(){
+    Write-Host ' '     # for blank line
+    & git branch --all
+}
+
+# This mimics linux touch command.
+function touch(){
+	param (
+		[Parameter (Mandatory=$true)]
+		[ValidatePattern("\w+(.ps1|.txt|.py)")]
+		[string]$newfilename
+	)
+    Write-Host ' '     # for blank line
+    & write-output 'Clear-Host ' >  $newfilename
 }
